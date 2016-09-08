4be11cfc0e7441783abe6c535d8e5d40d95b3b58
Quang Ty Vo <quangdvo@gmail.com>
2016-09-08 09:56:53 -0400

    Person3 method added
    Completed required method in Person3.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  String revStr = "";
+		for (char a : input) {
+            revStr = a + revStr;
+        }
+	  return revStr;
 	}
 }
