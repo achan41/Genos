eb24229b480b3191fbe795413a71e8dfd4bd1495
koyler3 <kmoyler@gmail.com>
2016-09-20 21:30:58 -0400

    Add files via upload

---
+++ CleanWaterApp/src/MVC/MainFXApplication.java
@@ -0,0 +1,31 @@
+package MVC;
+
+
+//import controller.WelcomeScreenController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Kevin on 9/20/2016.
+ */
+public class MainFXApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("WelcomeScreen.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("Clean Water App: Welcome!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ CleanWaterApp/src/MVC/WelcomeScreenController.java
@@ -0,0 +1,22 @@
+package MVC;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
+
+/**
+ * Created by Kevin on 9/20/2016.
+ *
+ * Controller for the welcome page
+ */
+public class WelcomeScreenController {
+    @FXML private Text actionTarget;
+
+    @FXML protected void handleLoginButtonAction(ActionEvent event) {
+        //Login functionality, go to app if successful
+    }
+    @FXML protected void handleRegisterButtonAction(ActionEvent event) {
+        //Go to registration page
+    }
+}
