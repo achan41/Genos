60038e0d7ff733a497b73550966676a7ba40cddd
Quang Vo <quangdvo@gmail.com>
2016-09-22 20:37:31 -0400

    Login Screen Added (Simple Measure)
    Added login screen distinction from welcome screen. Lost
    javadoc/comments in overwrite failure, will add later.

--- CleanWaterApp/src/MVC/WelcomeScreenController.java
+++ CleanWaterApp/src/MVC/WelcomeScreenController.java
@@ -2,50 +2,86 @@ package MVC;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-/**
- * Created by Kevin on 9/20/2016.
- *
- * Controller for the welcome page
- */
 public class WelcomeScreenController {
-    @FXML
-    private TextField usernameField;
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private Label messageText;
-    private Text actionTarget;
-
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Label messageText, usernameLabel, passwordLabel;
+    @FXML private Text actionTarget, welcomeText;
+    @FXML private Button cancelButton, loginButton, loginScreenButton, registerScreenButton;
+    private String welcomeMSG = "Welcome";
     private UserDatabase database = new UserDatabase();
 
 
-
-    @FXML protected void handleLoginButtonAction(ActionEvent event) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        try {
-            boolean validLogin = database.login(username, password);
-            if (validLogin) {
-                messageText.setText("Successfully logged in, " + usernameField.getText() + "!");
-            } else {
-                messageText.setText("Your password is incorrect!");
-                passwordField.clear();
-            }
-        } catch (NullPointerException e) {
-            messageText.setText("This user does not exist");
-            passwordField.clear();
-        }
-
-
-
+    @FXML
+    protected void handleLoginScreenButtonAction(ActionEvent e) {
+        this.setRegisterVisible(false);
+        this.setLoginVisible(true);
     }
-    @FXML protected void handleRegisterButtonAction(ActionEvent event) {
-        //Go to registration page
+
+    @FXML
+    protected void handleRegisterScreenButtonAction(ActionEvent e) {
+        this.setLoginVisible(false);
+        this.setRegisterVisible(true);
+    }
+
+    @FXML
+    protected void handleLoginButtonAction(ActionEvent event) {
+        try {
+            boolean validLogin = this.database.login(this.usernameField.getText(), this.passwordField.getText());
+            if (validLogin) {
+                this.messageText.setText("Successfully logged in, " + this.usernameField.getText() + "!");
+            } else {
+                this.messageText.setText("Your password is incorrect!");
+                this.passwordField.clear();
+            }
+        }
+        catch (NullPointerException e) {
+            this.messageText.setText("This user does not exist");
+            this.passwordField.clear();
+        }
+    }
+
+    @FXML
+    protected void handleRegisterButtonAction(ActionEvent event) {
+    }
+
+    @FXML
+    protected void handleCancelButtonAction(ActionEvent event) {
+        this.setLoginVisible(false);
+        this.setRegisterVisible(false);
+        this.welcomeText.setText(welcomeMSG);
+    }
+
+    /**
+     * sets login FXML objects to visible boolean
+     * @param visible whether or not login form is visible
+     */
+    @FXML
+    protected void setLoginVisible(boolean visible) {
+        welcomeText.setText("Login Screen");
+        usernameField.clear();
+        passwordField.clear();
+        usernameLabel.setVisible(visible);
+        passwordLabel.setVisible(visible);
+        usernameField.setVisible(visible);
+        passwordField.setVisible(visible);
+        cancelButton.setVisible(visible);
+        loginButton.setVisible(visible);
+        loginScreenButton.setVisible(!visible);
+        registerScreenButton.setVisible(!visible);
+    }
+
+    /**
+     * sets register FXML objects to visible boolean
+     * @param visible whether or not register form is visible
+     */
+    @FXML
+    protected void setRegisterVisible(boolean visible) {
     }
 }
