3872663b62c0aac9c17c6eebd462d84c87c3909f
Tori Walker <twalker61@gatech.edu>
2016-09-23 16:06:36 -0400

    Didn't change anything

--- CleanWaterApp/src/MVC/MainFXApplication.java
+++ CleanWaterApp/src/MVC/MainFXApplication.java
@@ -25,7 +25,6 @@ public class MainFXApplication extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) {launch(args);
     }
 }
