a8bb6fd2a9dc40626f81ea2231773a2f2ace4ad2
dionisiatara <taradionisia@yahoo.com>
2016-10-11 16:45:34 -0400

    Added water types class

---
+++ src/model/WaterType.java
@@ -0,0 +1,44 @@
+package model;
+
+/**
+ * Created by dionisiatara on 10/11/16.
+ */
+public enum WaterType {
+    Bottled ("Bottled", "BTL"),
+    Well ("Well", "WLL"),
+    Stream ("Stream", "STR"),
+    Lake ("Lake", "LK"),
+    Spring ("Spring", "SPR"),
+    Other ("Other", "OTH");
+
+    private final String name;
+    private final String charRep;
+
+    /**
+     * Counstructor for AccountType enumeration
+     * @param cName
+     * @param cRep
+     */
+    WaterType(String cName, String cRep) {
+        name = cName;
+        charRep = cRep;
+    }
+
+    /**
+     * returns the full string name
+     * @return full name string
+     */
+    public String getName() {return name;}
+
+    /**
+     * returns the shortened char representation
+     * @return char rep string
+     */
+    public String getCharRep() {return charRep;}
+
+    /**
+     * returns the shortened char rep
+     * @return char rep string
+     */
+    public String toString() {return name;}
+}
